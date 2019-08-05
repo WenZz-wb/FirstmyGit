da3a7e7a2b8a108845fbc32a2108dfef024905b1
WenZz-wb <374080573@qq,com>
2019-08-05 13:36:26 +0800

    更新

--- src/main/java/com/hzit/App.java
+++ src/main/java/com/hzit/App.java
@@ -8,9 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("0.0");
         System.out.println( "Hello World!" );
         System.out.println("你好！！");
         System.out.println("再见！！");
+        System.out.println("再见了");
 
     }
 }
