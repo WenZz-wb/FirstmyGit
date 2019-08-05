9bb46e91958c70aee860c80bb65246797d83fbf3
WenZz-wb <374080573@qq,com>
2019-08-05 12:05:54 +0800

    第一次上传

---
+++ src/main/java/com/hzit/App.java
@@ -0,0 +1,16 @@
+package com.hzit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("你好！！");
+        System.out.println("再见！！");
+
+    }
+}
